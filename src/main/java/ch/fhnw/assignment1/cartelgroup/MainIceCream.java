dd461a053cc36c1c4b79b5223157da8cce2fbeac
catherine790 <catherine.spaeti@students.fhnw.ch>
2025-04-29 15:45:11 +0200

    new

--- src/main/java/ch/fhnw/assignment1/cartelgroup/MainIceCream.java
+++ src/main/java/ch/fhnw/assignment1/cartelgroup/MainIceCream.java
@@ -1,4 +1,22 @@
 package ch.fhnw.assignment1.cartelgroup;
 
-public class MainIceCream {
-}
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+    public class MainIceCream extends Application {
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/icecream_ui.fxml"));
+            primaryStage.setTitle("Ice Cream Cashier");
+            primaryStage.setScene(new Scene(root, 500, 400));
+            primaryStage.show();
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+    }
+
