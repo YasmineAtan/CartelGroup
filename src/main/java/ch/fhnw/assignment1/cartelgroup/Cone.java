e06079f15ba22f321f3ead366f2f01e3b371def2
catherine790 <catherine.spaeti@students.fhnw.ch>
2025-04-29 15:40:29 +0200

    new

--- src/main/java/ch/fhnw/assignment1/cartelgroup/Cone.java
+++ src/main/java/ch/fhnw/assignment1/cartelgroup/Cone.java
@@ -1,4 +1,35 @@
 package ch.fhnw.assignment1.cartelgroup;
 
-public class Cone{
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cone {
+    private LocalDateTime orderDate;
+    private List<Scoop> scoops;
+
+    public Cone() {
+        this.orderDate = LocalDateTime.now();
+        this.scoops = new ArrayList<>();
+    }
+
+    public boolean addScoop(Scoop scoop) {
+        if (scoops.size() < 3) {
+            scoops.add(scoop);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Scoop> getScoops() {
+        return scoops;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public double getTotalGross() {
+        return scoops.stream().mapToDouble(Scoop::getGrossPrice).sum();
+    }
 }
