256648c56ed59ad171f6850314688e51e3b34f04
lucainnoc <luca.innocenti@students.fhnw.ch>
2025-04-29 15:39:45 +0200

    main add

--- src/main/java/ch/fhnw/assignment1/cartelgroup/MainIceCream.java
+++ src/main/java/ch/fhnw/assignment1/cartelgroup/MainIceCream.java
@@ -1,4 +1,26 @@
 package ch.fhnw.assignment1.cartelgroup;
 
-public class MainIceCream {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainIceCream extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/IceCreamUI.fxml"));
+            Scene scene = new Scene(loader.load());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Ice Cream Cashier");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
